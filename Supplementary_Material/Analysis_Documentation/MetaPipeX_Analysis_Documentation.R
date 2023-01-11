66ea349aa3b82145bab7760e5b4c1b9bcaf39df0
JensFuenderich <64157104+JensFuenderich@users.noreply.github.com>
2023-01-11 18:26:53 +0100

    fixed column issue
    custom multilab and repproj cols did not work

--- Supplementary_Material/Analysis_Documentation/MetaPipeX_Analysis_Documentation.R
+++ Supplementary_Material/Analysis_Documentation/MetaPipeX_Analysis_Documentation.R
@@ -3,7 +3,7 @@
 ## define path to 0_Input folder
 # the folder contains data necessary for this analysis
 
-Input_folder_path <- ".../0_Input"
+Input_folder_path <- "~/Downloads/MetaPipeX_folder 9/0_Input"
 
 ## load/install packages, including MetaPipeX (from github)
 if (!require("pacman")) install.packages("pacman")
@@ -23,16 +23,30 @@ if (transform_to_IPD$Filter != "no filter") {
   })
 }else{}
 
+## create custom multi-lab name
+if (transform_to_IPD$custum_MultiLab == "yes") {
+  Input_Data <- lapply(1:length(Input_Data), function(x){
+    Input_Data[[x]]$MultiLab <- rep(transform_to_IPD$MultiLab, times = nrow(Input_Data[[x]]))
+  })
+} else {}
+
+## create custom replication project name
+if (transform_to_IPD$custum_ReplicationProject == "yes") {
+  Input_Data <- lapply(1:length(Input_Data), function(x){
+    Input_Data[[x]]$ReplicationProject <- rep(transform_to_IPD$ReplicationProject, times = nrow(Input_Data[[x]]))
+  })
+} else {}
+
 ## applying the input to the MetaPipeX function
 # run full_pipeline
 example_MetaPipeX_output <- MetaPipeX::full_pipeline(data = Input_Data,
-                                                     MultiLab = transform_to_IPD$MultiLab,
-                                                     ReplicationProject = transform_to_IPD$ReplicationProject,
+                                                     MultiLab = if (transform_to_IPD$custum_MultiLab == "yes") {"MultiLab"} else {transform_to_IPD$MultiLab},
+                                                     ReplicationProject = if (transform_to_IPD$custum_ReplicationProject == "yes") {"ReplicationProject"} else {transform_to_IPD$ReplicationProject},
                                                      Replication = transform_to_IPD$Replication,
                                                      DV = transform_to_IPD$DV,
                                                      Group = transform_to_IPD$Group,
                                                      # output_path = file.path(paste0(getwd(), "/")),
                                                      # folder_name = "MetaPipeX_Output"
-                                                     )
+)
 # view output
 View(example_MetaPipeX_output)
